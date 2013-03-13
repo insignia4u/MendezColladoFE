86e3b7da5f2a478019afd1d6af40a80408cc2052
Rafael Alejandro Lizardo Celiz <lizardoceliz@gmail.com>
2013-03-13 18:51:08 -0300

    [Home page]
      - fixs in header slider
      - basic body home page
      - basic footer partial

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,18 @@
 //= require jquery
 //= require modernizr
 //= require foundation
+//= require foundation.orbit
+
+$(document).ready(function() {
+  // $(document).foundation();
+
+  $(document).foundation('orbit', {bullets: true, timer_speed: 4000});
+
+  // Override Slider Header
+  $(".orbit-next, .orbit-prev").css({display: "none"});
+  $(".orbit-timer").css({display: "none"});
+  $(".orbit-slide-number").remove();
+  $(".orbit-bullets li").each( function() {
+    $(this).text($(this).attr("data-orbit-slide-number"));
+  } );
+});
